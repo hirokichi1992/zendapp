f3ef4c6420eab8f3f8d05c49aedfba37c9148784
hirokichi1992 <plastar14105@gmail.com>
2019-04-08 22:10:03 +0900

    second commit

---
+++ controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'Zend/Controller/Action.php';
+
+class IndexController extends Zend_Controller_Action{
+    public function indexAction(){
+    //ここにアクションの処理を記述
+    }
+    public function hogeAction(){
+    //ここにアクションの処理を記述
+    }
+    
+}
